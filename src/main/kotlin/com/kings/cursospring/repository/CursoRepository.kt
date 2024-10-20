ea10fbeb554f9fa89b874c42093c7f3e4c79b24f
Kings <systemskings@gmail.com>
2024-10-20 10:51:36 -0300

    remove warnings

--- src/main/kotlin/com/kings/cursospring/repository/CursoRepository.kt
+++ src/main/kotlin/com/kings/cursospring/repository/CursoRepository.kt
@@ -3,5 +3,4 @@ package com.kings.cursospring.repository
 import com.kings.cursospring.model.Curso
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CursoRepository: JpaRepository<Curso, Long> {
-}
+interface CursoRepository: JpaRepository<Curso, Long>
--- src/main/kotlin/com/kings/cursospring/repository/TopicoRepository.kt
+++ src/main/kotlin/com/kings/cursospring/repository/TopicoRepository.kt
@@ -3,5 +3,4 @@ package com.kings.cursospring.repository
 import com.kings.cursospring.model.Topico
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TopicoRepository: JpaRepository<Topico, Long> {
-}
+interface TopicoRepository: JpaRepository<Topico, Long>
