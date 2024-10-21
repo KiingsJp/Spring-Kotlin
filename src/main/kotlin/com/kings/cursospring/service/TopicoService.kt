2e3b9d305d1b3c27469e43cc82d9ef579abd7e1c
Kings <systemskings@gmail.com>
2024-10-21 11:39:59 -0300

    adicionado console h2 e entityManager

--- src/main/kotlin/com/kings/cursospring/service/TopicoService.kt
+++ src/main/kotlin/com/kings/cursospring/service/TopicoService.kt
@@ -10,14 +10,15 @@ import com.kings.cursospring.repository.TopicoRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import java.util.stream.Collectors
+import javax.persistence.EntityManager
 
 @Service
 class TopicoService(
     private val repository: TopicoRepository,
     private val topicoViewMapper: TopicoViewMapper,
     private val topicoFormMapper: TopicoFormMapper,
-    private val notFoundMessage: String = "Topico nao encontrado!"
+    private val notFoundMessage: String = "Topico nao encontrado!",
+    private val entityManager: EntityManager
 ) {
     fun listar(nomeCurso: String?, paginacao: Pageable): Page<TopicoView> {
         val topicos = if (nomeCurso == null) repository.findAll(paginacao) else repository.findByCursoNome(nomeCurso, paginacao)
